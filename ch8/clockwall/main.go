7dfd98d41e9d06cb7be5152d69e558a1ff7de878
yykhomenko <yykhomenko@gmail.com>
2020-06-27 01:25:44 +0300

    added clockwall config

--- ch8/clockwall/main.go
+++ ch8/clockwall/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":8080")
+
+	if len(os.Args) < 2 {
+		log.Fatalln(errors.New("must be at least one param"))
+	}
+
+	for _, arg := range os.Args[1:] {
+		kv := strings.Split(arg, "=")
+		name, addr := kv[0], kv[1]
+		clock(name, addr)
+	}
+
+}
+
+func clock(name, addr string) {
+	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
