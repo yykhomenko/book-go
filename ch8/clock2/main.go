35d6dd2925014f6f67a89fbeb872c7a838c1d647
yykhomenko <yykhomenko@gmail.com>
2020-06-27 00:30:34 +0300

    init

---
+++ ch8/clock2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
