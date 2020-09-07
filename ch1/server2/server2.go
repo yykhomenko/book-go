e04dd93fc5136fcf2b0d1ee522ba58f041ac15e8
yykhomenko <yykhomenko@gmail.com>
2020-09-07 10:44:47 +0300

    added server2.go

---
+++ ch1/server2/server2.go
@@ -0,0 +1,34 @@
+// Server2 is minimal HTTP "echo" server with request counter.
+// go run server2.go
+// http :8000
+// http :8000/count
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
