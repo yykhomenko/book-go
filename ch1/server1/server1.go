af28df025de5dbbdffdf1ab445a2bbcf444411e3
yykhomenko <yykhomenko@gmail.com>
2020-09-07 10:29:04 +0300

    added server1.go

---
+++ ch1/server1/server1.go
@@ -0,0 +1,19 @@
+// Server1 is minimal HTTP "echo" server.
+// go run server1.go
+// http :8000/hello
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
