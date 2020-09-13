6deed1f345dad27adae9cc4e6900409143eb292f
yykhomenko <yykhomenko@gmail.com>
2020-09-13 16:28:07 +0300

    added test to popcount

--- ch2/popcount/popcount_test.go
+++ ch2/popcount/popcount_test.go
@@ -1,7 +1,16 @@
-// go test -bench .
+// go test
+// go test -bench . -benchmem
 package popcount
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPopCount(t *testing.T) {
+	assert.Equal(t, 4, PopCount(15))
+}
 
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
