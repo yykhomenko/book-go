e8b50f69361b5bde74e24d5f813d2fb27a29be26
yykhomenko <yykhomenko@gmail.com>
2020-09-22 19:14:23 +0300

    added ex 3.1

---
+++ ch3/ex_3_1_surface/surface.go
@@ -0,0 +1,70 @@
+// Surface computes SVG-representation from three-dimension graph of function.
+// go run surface.go > example.svg
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func main() {
+	fmt.Fprintf(os.Stdout,
+		"<svg xmlns='http://www.w3.org/2000/svg' "+
+			"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+			"width='%d' height='%d'>",
+		width, height)
+
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+
+			if isNaN(ax, ay, bx, by, cx, cy, dx, dy) {
+				continue
+			}
+
+			fmt.Fprintf(os.Stdout,
+				"<polygon points='%g,%g %g,%g %g,%g %g,%g'/>",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+
+	fmt.Fprintln(os.Stdout, "</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	z := f(x, y)
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r) / r
+}
+
+func isNaN(args ...float64) bool {
+	for _, arg := range args {
+		if math.IsNaN(arg) {
+			return true
+		}
+	}
+	return false
+}
