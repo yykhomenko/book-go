53c791e1d86ce356e6ec288b4b89dfa0cc8a9c72
yykhomenko <yykhomenko@gmail.com>
2020-10-05 22:55:01 +0300

    fix ex3.12

--- ch3/ex_3_12_anagram/anagram.go
+++ ch3/ex_3_12_anagram/anagram.go
@@ -1,6 +1,6 @@
-// go run anagram.go ana //true
-// go run anagram.go xyyx //true
-// go run anagram.go hello //false
+// go run anagram.go atomic cmiato //true
+// go run anagram.go one eno //true
+// go run anagram.go hello heloo //false
 package main
 
 import (
@@ -9,14 +9,31 @@ import (
 )
 
 func main() {
-	fmt.Fprintln(os.Stdout, isAnagram(os.Args[1]))
+	fmt.Fprintln(os.Stdout, isAnagram(os.Args[1], os.Args[2]))
 }
 
-func isAnagram(s string) bool {
-	for i := 0; i < len(s)/2; i++ {
-		if s[i] != s[len(s)-1-i] {
+func isAnagram(a, b string) bool {
+	aFreq := make(map[rune]int)
+	for _, r := range a {
+		aFreq[r]++
+	}
+
+	bFreq := make(map[rune]int)
+	for _, r := range b {
+		bFreq[r]++
+	}
+
+	for k, v := range aFreq {
+		if bFreq[k] != v {
 			return false
 		}
 	}
+
+	for k, v := range bFreq {
+		if aFreq[k] != v {
+			return false
+		}
+	}
+
 	return true
 }
