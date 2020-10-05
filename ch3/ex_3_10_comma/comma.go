bfe617adc69d028afbb6293361652151032b1942
yykhomenko <yykhomenko@gmail.com>
2020-10-05 20:35:13 +0300

    ref

--- ch3/ex_3_10_comma/comma.go
+++ ch3/ex_3_10_comma/comma.go
@@ -8,19 +8,24 @@ import (
 
 func comma(s string) string {
 	const groupSize = 3
+
 	n := len(s)
 	if n <= groupSize {
 		return s
 	}
+
 	var buf bytes.Buffer
+
 	offset := n % groupSize
 	buf.WriteString(s[:offset])
+
 	for i := offset; i < n; i += groupSize {
 		if i != 0 {
 			buf.WriteByte(',')
 		}
 		buf.WriteString(s[i : i+groupSize])
 	}
+
 	return buf.String()
 }
 
