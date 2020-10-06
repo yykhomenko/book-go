a0f9edfc5b1da8f16e61ddbe55fd594626a3a148
yykhomenko <yykhomenko@gmail.com>
2020-10-06 19:00:08 +0300

    added example

--- ch4/append/append.go
+++ ch4/append/append.go
@@ -1,4 +1,15 @@
-package append
+package main
+
+import "fmt"
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+}
 
 func appendInt(x []int, y int) []int {
 	var z []int
