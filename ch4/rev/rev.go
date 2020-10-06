8abe7e053579e724f937a22ccead3ca612b07e1f
yykhomenko <yykhomenko@gmail.com>
2020-10-06 17:29:07 +0300

    added examples

--- ch4/rev/rev.go
+++ ch4/rev/rev.go
@@ -1,4 +1,20 @@
-package rev
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(a)
+
+	reverse(a[:])
+	fmt.Println(a)
+	reverse(a[:])
+
+	reverse(a[:2])
+	reverse(a[2:])
+	reverse(a[:])
+	fmt.Println(a)
+}
 
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
