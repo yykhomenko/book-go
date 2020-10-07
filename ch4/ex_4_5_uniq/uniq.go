9e4c9d75f9ee3689e2673a7c26106de8edc75a79
yykhomenko <yykhomenko@gmail.com>
2020-10-07 12:05:01 +0300

    simplified ex4.5 test

--- ch4/ex_4_5_uniq/uniq.go
+++ ch4/ex_4_5_uniq/uniq.go
@@ -3,11 +3,10 @@ package ex_4_5_uniq
 func uniq(strs []string) []string {
 	var w int
 	for _, s := range strs {
-		if strs[w] == s {
-			continue
+		if strs[w] != s {
+			w++
+			strs[w] = s
 		}
-		w++
-		strs[w] = s
 	}
 	return strs[:w+1]
 }
