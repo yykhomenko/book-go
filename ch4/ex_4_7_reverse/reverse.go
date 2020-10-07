a087ab69bb99e1cf1d7b123ddbd3bc7c8382388a
yykhomenko <yykhomenko@gmail.com>
2020-10-07 16:19:58 +0300

    added ex4.7

---
+++ ch4/ex_4_7_reverse/reverse.go
@@ -0,0 +1,21 @@
+// Reverse all the runes, and then the entire slice.
+package main
+
+import "unicode/utf8"
+
+// ex4.7
+func revUTF8(b []byte) []byte {
+	for i := 0; i < len(b); {
+		_, size := utf8.DecodeRune(b[i:])
+		rev(b[i : i+size])
+		i += size
+	}
+	rev(b)
+	return b
+}
+
+func rev(b []byte) {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+}
