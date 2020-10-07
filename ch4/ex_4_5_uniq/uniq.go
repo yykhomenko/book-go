daf32a2eefdc30957850787553345add01ebbf7c
yykhomenko <yykhomenko@gmail.com>
2020-10-07 11:35:39 +0300

    added ex4.5 uniq

---
+++ ch4/ex_4_5_uniq/uniq.go
@@ -0,0 +1,13 @@
+package ex_4_5_uniq
+
+func uniq(strs []string) []string {
+	var w int
+	for _, s := range strs {
+		if strs[w] == s {
+			continue
+		}
+		w++
+		strs[w] = s
+	}
+	return strs[:w+1]
+}
