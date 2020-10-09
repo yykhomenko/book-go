ce5e9c7af0e6c62f32450d29a20863345eb4367c
yykhomenko <yykhomenko@gmail.com>
2020-10-09 21:59:26 +0300

    added issues.go

---
+++ ch4/issues/issues.go
@@ -0,0 +1,22 @@
+// go run issues.go golang
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/yykhomenko/book-gopl/ch4/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d themes\n", result.TotalCount)
+
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
