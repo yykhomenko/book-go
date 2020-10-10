87cbbb80370e7946350f6a1f4f72b1f1cf6b169d
yykhomenko <yykhomenko@gmail.com>
2020-10-10 21:34:28 +0300

    ref

--- ch4/ex_4_11_issues/main.go
+++ ch4/ex_4_11_issues/main.go
@@ -1,6 +1,9 @@
 // go build -o issues && ./issues search golang
-// go build -o issues && ./issues edit OWNER REPO NUMBER
+// go build -o issues && ./issues list yykhomenko test
+// go build -o issues && ./issues get yykhomenko test 3
 // go build -o issues && export GITHUB_USER=USER && export GITHUB_PASS=PASS && ./issues create yykhomenko test
+// go build -o issues && export GITHUB_USER=USER && export GITHUB_PASS=PASS && ./issues update yykhomenko test 3
+// go build -o issues && export GITHUB_USER=USER && export GITHUB_PASS=PASS && ./issues close yykhomenko test 3
 package main
 
 import (
@@ -12,7 +15,8 @@ var usage = "usage: issues search|list|create|read|update|close TERM|(OWNER REPO
 
 func main() {
 	if len(os.Args) < 3 {
-		usageDie()
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
 	}
 
 	cmd := os.Args[1]
@@ -24,10 +28,10 @@ func main() {
 		switch cmd {
 		case "list":
 			getIssues(args[0], args[1])
-		case "create":
-			createIssue(args[0], args[1], args[2])
 		case "get":
 			getIssue(args[0], args[1], args[2])
+		case "create":
+			createIssue(args[0], args[1], args[2])
 		case "update":
 			updateIssue(args[0], args[1], args[2])
 		case "close":
@@ -36,9 +40,4 @@ func main() {
 			fmt.Fprintf(os.Stderr, "unknown command: %q, use: %s\n", cmd, usage)
 		}
 	}
-}
-
-func usageDie() {
-	fmt.Fprintln(os.Stderr, usage)
-	os.Exit(1)
 }
