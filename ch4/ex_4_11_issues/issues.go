9d181f74b98ac167c95dc7e1f65a11d13b3e61ad
yykhomenko <yykhomenko@gmail.com>
2020-10-10 12:01:44 +0300

    added ex4.11 checkpoint

---
+++ ch4/ex_4_11_issues/issues.go
@@ -0,0 +1,21 @@
+package main
+
+func searchIssue(args []string) {
+
+}
+
+func createIssue(owner string, repo string, number string) {
+
+}
+
+func readIssue(owner string, repo string, number string) {
+
+}
+
+func updateIssue(owner string, repo string, number string) {
+
+}
+
+func closeIssue(owner string, repo string, number string) {
+
+}
---
+++ ch4/ex_4_11_issues/main.go
@@ -0,0 +1,45 @@
+// go run issues.go search github golang
+// go run issues.go edit OWNER REPO NUMBER
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var usage = "usage: issues search|create|read|update|close TERM|(OWNER REPO NUMBER)]"
+
+func main() {
+	if len(os.Args) < 3 {
+		usageDie()
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	if cmd == "search" {
+		searchIssue(args)
+	} else {
+		if len(os.Args) != 5 {
+			usageDie()
+		}
+		owner, repo, number := args[0], args[1], args[2]
+		switch cmd {
+		case "create":
+			createIssue(owner, repo, number)
+		case "read":
+			readIssue(owner, repo, number)
+		case "update":
+			updateIssue(owner, repo, number)
+		case "close":
+			closeIssue(owner, repo, number)
+		default:
+			fmt.Fprintf(os.Stderr, "unknown command: %q, use: %s\n", cmd, usage)
+		}
+	}
+}
+
+func usageDie() {
+	fmt.Fprintln(os.Stderr, usage)
+	os.Exit(1)
+}
