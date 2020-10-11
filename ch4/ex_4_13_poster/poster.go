2c7113067b2d365502d6207a154dfed0b048fa2a
yykhomenko <yykhomenko@gmail.com>
2020-10-11 15:15:58 +0300

    added ex4.13 done

--- ch4/ex_4_13_poster/poster.go
+++ ch4/ex_4_13_poster/poster.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const usage = `poster MOVIE_TITLE`
@@ -17,12 +19,24 @@ type Movie struct {
 	Poster string
 }
 
+func (m Movie) Filename() string {
+	i := strings.LastIndex(m.Poster, ".")
+	return fmt.Sprintf("%s_(%s)%s", m.Title, m.Year, m.Poster[i:])
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal(usage)
 	}
 
-	fmt.Println(GetMovie(os.Args[1]))
+	m, err := GetMovie(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := WritePoster(m); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func GetMovie(title string) (*Movie, error) {
@@ -43,4 +57,34 @@ func GetMovie(title string) (*Movie, error) {
 	}
 
 	return movie, nil
+}
+
+func WritePoster(m *Movie) error {
+	resp, err := http.Get(m.Poster)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unable to get poster: %v\n", resp.Status)
+	}
+
+	f, err := os.Create(m.Filename())
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(f)
+	r := bufio.NewReader(resp.Body)
+
+	if _, err = w.ReadFrom(r); err != nil {
+		return err
+	}
+
+	if err := w.Flush(); err != nil {
+		return err
+	}
+
+	return nil
 }
