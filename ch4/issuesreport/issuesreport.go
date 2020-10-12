b4fc876750e2ff97c80040109dfacc994ab22b16
yykhomenko <yykhomenko@gmail.com>
2020-10-12 09:38:19 +0300

    added issuereport.go

---
+++ ch4/issuesreport/issuesreport.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/yykhomenko/book-gopl/ch4/github"
+)
+
+const templ = `{{.TotalCount}} themes:
+{{range .Items}}-------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+var report = template.
+	Must(template.
+		New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ))
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
