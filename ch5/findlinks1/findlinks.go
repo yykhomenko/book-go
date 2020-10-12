43220dbb7e004793faa6a8e38288dd8a580652e5
yykhomenko <yykhomenko@gmail.com>
2020-10-12 21:13:48 +0300

    added findlinks.go

--- ch5/findlinks1/findlinks.go
+++ ch5/findlinks1/findlinks.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+
+	f, err := os.Open("example.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	doc, err := html.Parse(f)
+	// doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Fprintln(os.Stdout, link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode {
+		if n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+				}
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = n.NextSibling {
+		links = visit(links, c)
+	}
+
+	return links
+}
