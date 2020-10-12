4e0e7277c63644aea3195142d7762667381387ab
yykhomenko <yykhomenko@gmail.com>
2020-10-12 23:20:08 +0300

    added ex5.1

---
+++ ch5/ex_5_1_findlinks/findlinks.go
@@ -0,0 +1,43 @@
+// fetch golang.org | go run findlinks.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Fprintln(os.Stdout, link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode {
+		if n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+				}
+			}
+		}
+	}
+
+	if n.FirstChild != nil {
+		links = visit(links, n.FirstChild)
+	}
+
+	if n.NextSibling != nil {
+		links = visit(links, n.NextSibling)
+	}
+
+	return links
+}
