21fc4a4ac560c277d4db43bc283bdd6e80699e33
yykhomenko <yykhomenko@gmail.com>
2020-10-13 18:07:53 +0300

    added ex5.5

--- ch5/ex_5_5_countWI/countWI.go
+++ ch5/ex_5_5_countWI/countWI.go
@@ -1,14 +1,25 @@
+// go run countWI.go https://golang.org
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "count %s: %v\n", url, err)
+			continue
+		}
+		fmt.Println(words, images)
+	}
 }
 
 func CountWordsAndImages(url string) (words, images int, err error) {
@@ -29,5 +40,32 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 }
 
 func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+
+	if n.Type == html.TextNode {
+		data := strings.TrimSpace(n.Data)
+		if len(data) != 0 {
+			in := bufio.NewScanner(strings.NewReader(data))
+			in.Split(bufio.ScanWords)
+			for in.Scan() {
+				words++
+			}
+		}
+	}
+
+	if n.FirstChild != nil {
+		w, i := countWordsAndImages(n.FirstChild)
+		words += w
+		images += i
+	}
+
+	if n.NextSibling != nil {
+		w, i := countWordsAndImages(n.NextSibling)
+		words += w
+		images += i
+	}
+
 	return
 }
