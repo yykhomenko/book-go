8003451ec99c60489b6a422d4f3c56df2fb58149
yykhomenko <yykhomenko@gmail.com>
2020-10-13 14:51:32 +0300

    added findlinks2

---
+++ ch5/findlinks2/findlinks.go
@@ -0,0 +1,62 @@
+// go run findlinks.go https://golang.org https://google.com
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("analyze %s as HTML: %v", url, err)
+	}
+
+	return visit(nil, doc), nil
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	if n.FirstChild != nil {
+		links = visit(links, n.FirstChild)
+	}
+
+	if n.NextSibling != nil {
+		links = visit(links, n.NextSibling)
+	}
+
+	return links
+}
