36bdbdbb74fc9d72b805bb705a5615b9995d1ca2
yykhomenko <yykhomenko@gmail.com>
2020-10-24 18:12:25 +0300

    added ex5.12

---
+++ ch5/ex_5_12_outline2/outline.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+	outline(doc)
+}
+
+func outline(doc *html.Node) {
+	var depth int
+	forEachNode(doc,
+		func(n *html.Node) {
+			if n.Type == html.ElementNode {
+				fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+				depth++
+			}
+		}, func(n *html.Node) {
+			if n.Type == html.ElementNode {
+				depth--
+				fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+			}
+		})
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
