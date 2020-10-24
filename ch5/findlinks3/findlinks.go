23142b615bc8f143827177a40602ac8dd58993bd
yykhomenko <yykhomenko@gmail.com>
2020-10-24 17:48:56 +0300

    added findlinks.go

---
+++ ch5/findlinks3/findlinks.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/yykhomenko/book-gopl/ch5/links"
+)
+
+func main() {
+	breadthFirst(crawl, os.Args[1:])
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func breadthFirst(f func(string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
