8c8859ca0f2d0193f5d85dad5384afacac00b5fa
yykhomenko <yykhomenko@gmail.com>
2020-10-25 11:05:04 +0200

    added crawlOnlyHosts

--- ch5/ex_5_13_crawl/crawl.go
+++ ch5/ex_5_13_crawl/crawl.go
@@ -4,22 +4,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/yykhomenko/book-gopl/ch5/links"
 )
 
 func main() {
-	breadthFirst(crawl, os.Args[1:])
+	urls := os.Args[1:]
+	breadthFirst(crawlOnlyHosts(urls), urls)
 }
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := links.Extract(url)
-	if err != nil {
-		log.Print(err)
+func crawlOnlyHosts(urls []string) func(string) []string {
+	var hosts []string
+	for _, u := range urls {
+		l, err := url.Parse(u)
+		if err != nil {
+			log.Fatalf("parse %s: %v/n", u, err)
+		}
+		hosts = append(hosts, l.Host)
 	}
-	return list
+
+	return func(url string) []string {
+		for _, host := range hosts {
+			if strings.Contains(url, host) {
+				fmt.Println(url)
+				urls, err := links.Extract(url)
+				if err != nil {
+					log.Print(err)
+				}
+				return urls
+			}
+		}
+		return nil
+	}
 }
 
 func breadthFirst(f func(string) []string, worklist []string) {
