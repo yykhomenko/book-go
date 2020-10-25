d951e4f25e5c681684952b88fd1b59476fc6e946
yykhomenko <yykhomenko@gmail.com>
2020-10-25 13:10:55 +0200

    added sum

--- ch5/sum/main.go
+++ ch5/sum/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println(sum())
 	fmt.Println(sum(3))
 	fmt.Println(sum(1, 2, 3, 4))
+	values := []int{1, 2, 3, 4}
+	fmt.Println(sum(values...))
+
+	var f func(...int)
+	var g func([]int)
+	fmt.Printf("%T\n", f)
+	fmt.Printf("%T\n", g)
+}
+
+func errorf(linenum int, format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "Pg. %d: ", linenum)
+	fmt.Fprintf(os.Stderr, format, args...)
+	fmt.Println(os.Stderr)
 }
 
 func sum(vals ...int) int {
