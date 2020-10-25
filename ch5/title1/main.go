4c1e8452d6da585de921798f363a4899a54c7856
yykhomenko <yykhomenko@gmail.com>
2020-10-25 15:13:06 +0200

    added title1

---
+++ ch5/title1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	title(os.Args[1])
+}
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s but not texl/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("analyze %s as HTML: %v", url, err)
+	}
+
+	visitNode := func(n *html.Node) bool {
+		if n.Type == html.ElementNode &&
+			n.Data == "title" &&
+			n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+			return true
+		}
+		return false
+	}
+
+	forEachNode(doc, visitNode, nil)
+
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node) bool) {
+	if pre != nil && pre(n) {
+		return
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil && post(n) {
+		return
+	}
+}
