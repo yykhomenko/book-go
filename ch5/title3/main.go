e095dbfdd0e49d5ef88b7287c6d08dd6d06d4523
yykhomenko <yykhomenko@gmail.com>
2020-10-29 09:51:05 +0200

    added title3

---
+++ ch5/title3/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+	defer func() {
+		switch p := recover(); p {
+		case nil: // no panic
+		case bailout{}:
+			err = fmt.Errorf("several title elements")
+		default:
+			panic(p)
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "" && n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{})
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+
+	if title == "" {
+		return "", fmt.Errorf("title not found")
+	}
+
+	return title, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
