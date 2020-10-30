05b45c3c9d3557625455df4fd664d4ba5abb13ae
yykhomenko <yykhomenko@gmail.com>
2020-10-30 18:44:13 +0200

    added intset test Add

---
+++ ch6/intset/intset_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	s := IntSet{}
+	s.Add(4)
+	s.Add(2)
+	assert.Equal(t, "{2 4}", s.String())
+}
+
+func TestHas(t *testing.T) {
+
+}
