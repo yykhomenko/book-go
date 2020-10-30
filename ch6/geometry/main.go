c1efb9283214aab6283b30cb12ab284a0dcd2684
yykhomenko <yykhomenko@gmail.com>
2020-10-30 10:31:58 +0200

    added ScaleBy func

--- ch6/geometry/main.go
+++ ch6/geometry/main.go
@@ -16,6 +16,11 @@ func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
 func (path Path) Distance() float64 {
 	sum := 0.0
 	for i := range path {
@@ -31,4 +36,12 @@ func main() {
 	q := Point{4, 6}
 	fmt.Println(Distance(p, q))
 	fmt.Println(p.Distance(q))
+
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
 }
