974a0b8bc2517ef1145e4cf8e2787e48ab8eba3a
yykhomenko <yykhomenko@gmail.com>
2020-10-30 18:53:20 +0200

    added TestUnionWith()

--- ch6/intset/intset_test.go
+++ ch6/intset/intset_test.go
@@ -20,3 +20,14 @@ func TestHas(t *testing.T) {
 	assert.True(t, x.Has(4))
 	assert.True(t, !x.Has(3))
 }
+
+func TestUnionWith(t *testing.T) {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	y.Add(9)
+	y.Add(42)
+	x.UnionWith(&y)
+	assert.Equal(t, "{1 9 42 144}", x.String())
+}
