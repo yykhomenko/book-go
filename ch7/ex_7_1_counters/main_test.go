a17814042e7de178457cbdf707ae2a99e31f82ca
yykhomenko <yykhomenko@gmail.com>
2020-10-31 18:16:54 +0200

    added ex 7.1 test

---
+++ ch7/ex_7_1_counters/main_test.go
@@ -0,0 +1,15 @@
+package counters
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWordCounter_Write(t *testing.T) {
+	var c WordCounter
+	const input = "Hello world 2020 5678"
+	c.Write([]byte(input))
+	c.Write([]byte(input))
+	assert.Equal(t, 8, int(c))
+}
