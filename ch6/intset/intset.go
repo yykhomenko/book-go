fcbfda590d5ad81a81441c33a29679174b32a527
yykhomenko <yykhomenko@gmail.com>
2020-10-31 10:04:10 +0200

    added DifferenceWith

--- ch6/intset/intset.go
+++ ch6/intset/intset.go
@@ -99,3 +99,13 @@ func (s *IntSet) IntersectWith(t *IntSet) {
 	}
 	s.words = words
 }
+
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
