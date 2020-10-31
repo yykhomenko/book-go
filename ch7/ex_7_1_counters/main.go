eece99aab0d443c16a5397d3eedfaafcfb8adc2d
yykhomenko <yykhomenko@gmail.com>
2020-10-31 18:30:03 +0200

    added LineCounter impl

--- ch7/ex_7_1_counters/main.go
+++ ch7/ex_7_1_counters/main.go
@@ -6,10 +6,25 @@ import (
 )
 
 type WordCounter int
+type LineCounter int
 
 func (c *WordCounter) Write(p []byte) (int, error) {
 	scanner := bufio.NewScanner(bytes.NewReader(p))
 	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		*c++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return len(p), nil
+}
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		*c++
 	}
