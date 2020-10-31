0dc41822b5ff86f2cf2d03fbdfdb6c84e0a2e039
yykhomenko <yykhomenko@gmail.com>
2020-10-31 18:19:52 +0200

    added LineCounter test

--- ch7/ex_7_1_counters/main_test.go
+++ ch7/ex_7_1_counters/main_test.go
@@ -7,8 +7,16 @@ import (
 )
 
 func TestWordCounter(t *testing.T) {
-	var c WordCounter
 	const input = "Hello world 2020 5678"
+	var c WordCounter
+	c.Write([]byte(input))
+	c.Write([]byte(input))
+	assert.Equal(t, 8, int(c))
+}
+
+func TestLineCounter(t *testing.T) {
+	const input = "Hello\nworld\n2020\n5678"
+	var c WordCounter
 	c.Write([]byte(input))
 	c.Write([]byte(input))
 	assert.Equal(t, 8, int(c))
