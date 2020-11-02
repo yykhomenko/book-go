2e27b7bc9e2e0879ac5f8bf48780ae142f933585
yykhomenko <yykhomenko@gmail.com>
2020-11-02 20:46:23 +0200

    added ex7.13 test

--- ch7/ex_7_13/string_test.go
+++ ch7/ex_7_13/string_test.go
@@ -1,16 +1,17 @@
 package eval
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCallString(t *testing.T) {
-	const input = "sin(x)+-25+sin(2)*2/10-3.14/sin(1/2)"
+	const input = "sin(x)+-25++30+sqrt(2)*2/10-3.14e0/pow(y/2,2)"
+	const expected = "sin(x) + -25 + +30 + sqrt(2)*2/10 - 3.14/pow(y/2, 2)"
 	expr, err := Parse(input)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(expr)
-
+	assert.Equal(t, expected, expr.String())
 }
