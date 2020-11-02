aae0f14a4a37fab66de47af2b07b5914e90f0ef9
yykhomenko <yykhomenko@gmail.com>
2020-11-02 14:48:45 +0200

    added create

--- ch7/ex_7_11_http/main.go
+++ ch7/ex_7_11_http/main.go
@@ -16,6 +16,7 @@ func main() {
 	db := NewDatabase(map[string]dollars{"shoes": 50, "socks": 5})
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/create", db.create)
 	http.HandleFunc("/update", db.update)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
@@ -34,6 +35,8 @@ func NewDatabase(prices map[string]dollars) *database {
 }
 
 func (db *database) list(w http.ResponseWriter, r *http.Request) {
+	db.Lock()
+	defer db.Unlock()
 	for item, price := range db.prices {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
@@ -41,6 +44,8 @@ func (db *database) list(w http.ResponseWriter, r *http.Request) {
 
 func (db *database) price(w http.ResponseWriter, r *http.Request) {
 	item := r.URL.Query().Get("item")
+	db.Lock()
+	defer db.Unlock()
 	price, ok := db.prices[item]
 	if !ok {
 		w.WriteHeader(http.StatusNotFound)
@@ -48,6 +53,27 @@ func (db *database) price(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db *database) create(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price, err := strconv.ParseFloat(r.URL.Query().Get("price"), 32)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unable to parse price: %v\n", err)
+		return
+	}
+
+	db.Lock()
+	defer db.Unlock()
+
+	if _, ok := db.prices[item]; ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "item already present: %q\n", item)
+		return
+	}
+	db.prices[item] = dollars(price)
 }
 
 func (db *database) update(w http.ResponseWriter, r *http.Request) {
