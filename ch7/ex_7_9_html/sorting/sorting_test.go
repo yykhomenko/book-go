fb5d51cb6f04b0657ddc003e4612212e90115a13
yykhomenko <yykhomenko@gmail.com>
2020-11-02 09:55:08 +0200

    added sorting test

---
+++ ch7/ex_7_9_html/sorting/sorting_test.go
@@ -0,0 +1,33 @@
+package sorting
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestByColumns(t *testing.T) {
+	// "Title", "Artist", "Album", "Year", "Length"
+	var tracks = []*Track{
+		{"Go", "Delilah", "From the Roots Up", 2012, Length("3m38s")},
+		{"Go", "Moby", "Moby", 1992, Length("3m37s")},
+		{"Go Ahead", "Alicia Keys", "As I Am", 2007, Length("4m36s")},
+		{"Ready 2 Go", "Martin Solveig", "Smash", 2011, Length("4m24s")},
+	}
+	var expected = []*Track{
+		{"Go", "Moby", "Moby", 1992, Length("3m37s")},
+		{"Go", "Delilah", "From the Roots Up", 2012, Length("3m38s")},
+		{"Go Ahead", "Alicia Keys", "As I Am", 2007, Length("4m36s")},
+		{"Ready 2 Go", "Martin Solveig", "Smash", 2011, Length("4m24s")},
+	}
+
+	cs := NewByColumns(tracks, 3)
+	cs.Select(LessYear)
+	cs.Select(LessTitle)
+
+	sort.Sort(cs)
+	cs.PrintTracks()
+
+	assert.Equal(t, expected, tracks)
+}
