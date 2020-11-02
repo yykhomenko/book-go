8eb48ff082a28a2bcde4eac0c50a4e7049902dea
yykhomenko <yykhomenko@gmail.com>
2020-11-02 15:50:36 +0200

    eval checkpoint

---
+++ ch7/eval/eval.go
@@ -0,0 +1,32 @@
+package main
+
+type Expr interface {
+	// Eval returns value of Expr in environment env.
+	Eval(env Env) float64
+}
+
+// Var is variable, for example x.
+type Var string
+
+// literal represents numbering constant, for example 3.14.
+type literal float64
+
+// unary represents expression with a unary operator.
+type unary struct {
+	op rune // '+' or '-'
+	x  Expr
+}
+
+// binary represents expression with a binary operator.
+type binary struct {
+	op   rune // '+', '-', '*' or '/'
+	x, y Expr
+}
+
+// call represents expression function invocation, for example six(x).
+type call struct {
+	fn   string // one of "pow", "sin", "sqrt"
+	args []Expr
+}
+
+type Env map[Var]float64
