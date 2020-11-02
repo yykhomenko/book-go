f30f745b8992402d9c6e8a35e18292f38cbd1cd1
yykhomenko <yykhomenko@gmail.com>
2020-11-02 16:51:31 +0200

    refactor

--- ch7/eval/eval.go
+++ ch7/eval/eval.go
@@ -5,32 +5,12 @@ import (
 	"math"
 )
 
-type Expr interface {
-	// Eval returns value of Expr in environment env.
-	Eval(env Env) float64
-}
-
-// Env contains values of Var
-type Env map[Var]float64
-
-// Var is variable, for example x.
-type Var string
-
 func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
-// literal represents numbering constant, for example 3.14.
-type literal float64
-
 func (l literal) Eval(_ Env) float64 {
 	return float64(l)
-}
-
-// unary represents expression with a unary operator.
-type unary struct {
-	op rune // '+' or '-'
-	x  Expr
 }
 
 func (u unary) Eval(env Env) float64 {
@@ -42,12 +22,6 @@ func (u unary) Eval(env Env) float64 {
 	default:
 		panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
 	}
-}
-
-// binary represents expression with a binary operator.
-type binary struct {
-	op   rune // '+', '-', '*' or '/'
-	x, y Expr
 }
 
 func (b binary) Eval(env Env) float64 {
@@ -63,12 +37,6 @@ func (b binary) Eval(env Env) float64 {
 	default:
 		panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 	}
-}
-
-// call represents expression function invocation, for example six(x).
-type call struct {
-	fn   string // one of "pow", "sin", "sqrt"
-	args []Expr
 }
 
 func (c call) Eval(env Env) float64 {
