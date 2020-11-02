b6347bbf8f2ba18d26b607309058e0aa011ba8f3
yykhomenko <yykhomenko@gmail.com>
2020-11-02 14:06:20 +0200

    added ex 7.11 update

---
+++ ch7/ex_7_11_http/main.go
@@ -0,0 +1,53 @@
+// go run main.go
+// http://localhost:8000/list
+// http://localhost:8000/price?item=socks
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, r *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "item not found: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price, err := strconv.ParseFloat(r.URL.Query().Get("price"), 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unable to parse price: %v\n", err)
+		return
+	}
+	db[item] = dollars(price)
+}
