b1a392f73200d289c4f81642fffa7c133e7e8ea5
yykhomenko <yykhomenko@gmail.com>
2020-11-03 11:42:53 +0200

    added ex7.14 min operator <a1, a2, ... an>

---
+++ ch7/ex_7_14/ast.go
@@ -0,0 +1,39 @@
+package eval
+
+type Expr interface {
+	Eval(env Env) float64          // Eval returns value of Expr in environment env.
+	Check(vars map[Var]bool) error // Check report about errors in Expr and add self Vars.
+	String() string
+}
+
+// Env contains values of Var
+type Env map[Var]float64
+
+// Var is variable, for example x.
+type Var string
+
+// literal represents numbering constant, for example 3.14.
+type literal float64
+
+// unary represents expression with a unary operator.
+type unary struct {
+	op rune // '+' or '-'
+	x  Expr
+}
+
+// binary represents expression with a binary operator.
+type binary struct {
+	op   rune // '+', '-', '*' or '/'
+	x, y Expr
+}
+
+// call represents expression function invocation, for example six(x).
+type call struct {
+	fn   string // one of "pow", "sin", "sqrt"
+	args []Expr
+}
+
+// call represents expression function invocation, for example six(x).
+type min struct {
+	args []Expr
+}
---
+++ ch7/ex_7_14/check.go
@@ -0,0 +1,69 @@
+package eval
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (l literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("incorrect unary operator: %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("incorrect binary operator: %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown func %q", c.fn)
+	}
+
+	if len(c.args) != arity {
+		return fmt.Errorf("call %s has %d instead %d agruments",
+			c.fn, len(c.args), arity)
+	}
+
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m min) Check(vars map[Var]bool) error {
+	if len(m.args) < 2 {
+		return fmt.Errorf("call %s has minimum %d args instead of %d args provided",
+			"min <x, y>", 2, len(m.args))
+	}
+
+	for _, arg := range m.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
