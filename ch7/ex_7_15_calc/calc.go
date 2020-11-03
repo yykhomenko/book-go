dc4ecf1153d527f29749a981cf9d3cc1ad689104
yykhomenko <yykhomenko@gmail.com>
2020-11-03 16:59:42 +0200

    added ex7.15

--- ch7/ex_7_15_calc/calc.go
+++ ch7/ex_7_15_calc/calc.go
@@ -5,20 +5,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/yykhomenko/book-gopl/ch7/eval"
 )
 
 func main() {
+	fmt.Println("Expression:")
 	s := bufio.NewScanner(os.Stdin)
 	if s.Scan(); s.Err() != nil {
 		log.Fatalf("input: %v", s.Err())
 	}
 
-	e, err := eval.Parse(s.Text())
+	expr, err := eval.Parse(s.Text())
 	if err != nil {
 		log.Fatalf("parse: %v", err)
 	}
 
-	fmt.Fprintln(os.Stdout, e)
+	vars := eval.Filter(expr, eval.FilterVars)
+	env := eval.Env{}
+
+	fmt.Println("Please input follow variables:")
+	for i := 0; i < len(vars); {
+		fmt.Printf("%s = ", vars[i])
+		if s.Scan(); s.Err() != nil {
+			fmt.Fprintf(os.Stderr, "input: %v\n", err)
+			continue
+		}
+		v, err := strconv.ParseFloat(s.Text(), 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "parse: %v\n", err)
+			continue
+		}
+		env[eval.Var(vars[i].String())] = v
+		i++
+	}
+
+	fmt.Printf("%s = %g", expr, expr.Eval(env))
 }
