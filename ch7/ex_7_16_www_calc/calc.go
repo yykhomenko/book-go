8d229a25d4d9858a06766add47c3784075d4d83d
yykhomenko <yykhomenko@gmail.com>
2020-11-03 18:06:51 +0200

    added ex7.16

---
+++ ch7/ex_7_16_www_calc/calc.go
@@ -0,0 +1,40 @@
+// http://localhost:8000/calc?expr=x*y*sin(x)&x=2&y=3
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/yykhomenko/book-gopl/ch7/eval"
+)
+
+func main() {
+	http.HandleFunc("/calc", func(w http.ResponseWriter, r *http.Request) {
+		exp := r.URL.Query().Get("expr")
+		if exp == "" {
+			http.Error(w, "param expr is empty", http.StatusBadRequest)
+			return
+		}
+
+		expr, err := eval.Parse(exp)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("parse expr %q: %v", exp, err), http.StatusBadRequest)
+			return
+		}
+
+		env := eval.Env{}
+		for _, v := range eval.Filter(expr, eval.FilterVars) {
+			value, err := strconv.ParseFloat(r.URL.Query().Get(v.String()), 64)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("parse param %q: %v", v, err), http.StatusBadRequest)
+				return
+			}
+			env[v.(eval.Var)] = value
+		}
+
+		fmt.Fprintln(w, expr.Eval(env))
+	})
+
+	http.ListenAndServe("localhost:8000", nil)
+}
