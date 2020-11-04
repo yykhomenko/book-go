3809f3fcd07877c0f9bf7bef45782dfadd8aef41
yykhomenko <yykhomenko@gmail.com>
2020-11-04 09:04:51 +0200

    ex8.2 handle conn

--- ch8/ex_8_2_ftp_server/main.go
+++ ch8/ex_8_2_ftp_server/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
+func main() {
+	cmd, err := net.Listen("tcp", ":21")
+	if err != nil {
+		log.Fatal()
+	}
+
+	for {
+		conn, err := cmd.Accept()
+		if err != nil {
+			continue
+		}
+		go handle(conn)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	if _, err := io.Copy(os.Stdout, conn); err != nil {
+		log.Fatal(err)
+	}
 }
