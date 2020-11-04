f70ad8c24d1de1072d66bd35293a2fd6abffbe97
yykhomenko <yykhomenko@gmail.com>
2020-11-04 08:20:49 +0200

    added ex8.1

---
+++ ch8/ex_8_1_clock/README.md
@@ -0,0 +1,14 @@
+# clock2
+
+Run:
+```bash
+go run ch8/clock2/main.go
+```
+Run with specific port:
+```bash
+go run ch8/clock2/main.go -port 80
+```
+Run with other timezone:
+```bash
+TZ=US/Eastern go run ch8/clock2/main.go
+```
---
+++ ch8/ex_8_1_clock/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+var (
+	port string
+)
+
+func init() {
+	flag.StringVar(&port, "port", "8080", "server port")
+	flag.Parse()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
