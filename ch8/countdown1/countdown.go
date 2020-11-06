e87b3190963f06587c3ff2ed17f1973a9fd13f83
yykhomenko <yykhomenko@gmail.com>
2020-11-06 11:35:07 +0200

    added countdown

---
+++ ch8/countdown1/countdown.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Begin countdown.")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		<-tick
+	}
+	// lanch()
+}
