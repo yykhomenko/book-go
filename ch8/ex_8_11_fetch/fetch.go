309d4bac24387d70019e4fd5042779b738e0f1b0
yykhomenko <yykhomenko@gmail.com>
2020-11-08 16:25:30 +0200

    added ex8.11

---
+++ ch8/ex_8_11_fetch/fetch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	result := make(chan []byte)
+	cancel := make(chan struct{})
+	for _, url := range os.Args[1:] {
+		go fetch(url, result, cancel)
+	}
+
+	select {
+	case b := <-result:
+		close(cancel)
+		os.Stdout.Write(b)
+	}
+}
+
+func fetch(url string, result chan []byte, cancel <-chan struct{}) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	req.Cancel = cancel
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "get %s: %s\n", url, resp.Status)
+		return
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	result <- b
+}
