b9314bd0ae4578720596ebcf7d4e2c8742c2db80
yykhomenko <yykhomenko@gmail.com>
2020-11-09 10:21:55 +0200

    added bank4

---
+++ ch9/bank4/bank.go
@@ -0,0 +1,36 @@
+package bank
+
+import "sync"
+
+var (
+	mu      sync.RWMutex
+	balance int
+)
+
+func Deposit(amount int) {
+	mu.Lock()
+	defer mu.Unlock()
+	balance += amount
+}
+
+func Balance() int {
+	mu.RLock()
+	defer mu.RUnlock()
+	return balance
+}
+
+func Withdraw(amount int) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(-amount)
+	if balance < 0 {
+		deposit(-amount)
+		return false
+	}
+	return false
+}
+
+// function call under lock only
+func deposit(amount int) {
+	balance += amount
+}
