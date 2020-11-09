e8a2de02f31239e14738e9317b24e537fa94de9f
yykhomenko <yykhomenko@gmail.com>
2020-11-09 09:36:33 +0200

    added ex9.1 impl

--- ch9/ex_9_1_bank/bank.go
+++ ch9/ex_9_1_bank/bank.go
@@ -1,17 +1,22 @@
 package bank
 
 var (
-	deposits       = make(chan int)
-	balances       = make(chan int)
-	withdraws      = make(chan int)
-	withdrawResult = make(chan bool)
+	deposits  = make(chan int)
+	balances  = make(chan int)
+	withdraws = make(chan withdrawal)
 )
+
+type withdrawal struct {
+	amount  int
+	success chan bool
+}
 
 func Deposit(amount int) { deposits <- amount }
 func Balance() int       { return <-balances }
 func Withdraw(amount int) bool {
-	withdraws <- amount
-	return <-withdrawResult
+	w := withdrawal{amount, make(chan bool)}
+	withdraws <- w
+	return <-w.success
 }
 
 func teller() {
@@ -23,17 +28,17 @@ func teller() {
 			if amount > 0 {
 				balance += amount
 			}
-		case amount := <-withdraws:
-			if amount < balance {
-				balance -= amount
-				withdrawResult <- true
+		case w := <-withdraws:
+			if w.amount < balance {
+				balance -= w.amount
+				w.success <- true
 			} else {
-				withdrawResult <- false
+				w.success <- false
 			}
 		}
 	}
 }
 
 func init() {
-	teller()
+	go teller()
 }
