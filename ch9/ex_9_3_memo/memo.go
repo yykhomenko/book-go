061df8178d5f7e7b212c88eac69e358d048dd855
yykhomenko <yykhomenko@gmail.com>
2020-11-09 17:40:46 +0200

    ex9.3 checkpoint

---
+++ ch9/ex_9_3_memo/memo.go
@@ -0,0 +1,69 @@
+// Memo cache function call results.
+package memo
+
+type Func func(key string, done <-chan struct{}) (interface{}, error)
+
+type entry struct {
+	res   result
+	ready chan struct{}
+}
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type request struct {
+	key      string
+	done     <-chan struct{}
+	response chan<- result
+}
+
+type Memo struct {
+	requests, cancels chan request
+}
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+func (memo *Memo) Get(key string, done <-chan struct{}) (interface{}, error) {
+	response := make(chan result)
+	req := request{key, done, response}
+	memo.requests <- req
+	res := <-response
+	select {
+	case <-done:
+		memo.cancels <- req
+	default:
+	}
+
+	return res.value, res.err
+}
+
+func (memo *Memo) Close() { close(memo.requests) }
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key, req.done)
+		}
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string, done <-chan struct{}) {
+	e.res.value, e.res.err = f(key, done)
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	<-e.ready
+	response <- e.res
+}
