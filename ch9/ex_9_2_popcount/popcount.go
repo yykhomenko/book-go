a307aee816c6aead6a3615e05d020f4c1dd39eb9
yykhomenko <yykhomenko@gmail.com>
2020-11-09 10:53:17 +0200

    added ex9.2 impl

---
+++ ch9/ex_9_2_popcount/popcount.go
@@ -0,0 +1,29 @@
+package popcount
+
+import "sync"
+
+// pc[i] - number of single bits in "i".
+var (
+	initPcOnce sync.Once
+	pc         [256]byte
+)
+
+func initPc() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns number of bits set.
+func PopCount(x uint64) int {
+	initPcOnce.Do(initPc)
+	return int(
+		pc[byte(x>>(0*8))] +
+			pc[byte(x>>(1*8))] +
+			pc[byte(x>>(2*8))] +
+			pc[byte(x>>(3*8))] +
+			pc[byte(x>>(4*8))] +
+			pc[byte(x>>(5*8))] +
+			pc[byte(x>>(6*8))] +
+			pc[byte(x>>(7*8))])
+}
