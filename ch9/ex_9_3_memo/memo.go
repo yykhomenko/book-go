d29005f6a974bd63a43d8f61102925ba59e45131
yykhomenko <yykhomenko@gmail.com>
2020-11-10 11:27:01 +0200

    added ex9.3 impl

--- ch9/ex_9_3_memo/memo.go
+++ ch9/ex_9_3_memo/memo.go
@@ -1,18 +1,24 @@
-// Memo cache function call results.
 package memo
 
+import (
+	"fmt"
+)
+
+// Func is the type of the function to memoize.
 type Func func(key string, done <-chan struct{}) (interface{}, error)
+
+// A result is the result of calling a Func.
+type result struct {
+	value interface{}
+	err   error
+}
 
 type entry struct {
 	res   result
 	ready chan struct{}
 }
 
-type result struct {
-	value interface{}
-	err   error
-}
-
+// A request is a message requesting that the Func be applied to key.
 type request struct {
 	key      string
 	done     <-chan struct{}
@@ -23,8 +29,9 @@ type Memo struct {
 	requests, cancels chan request
 }
 
+// New returns a memoization of f.  Clients must subsequently call Close.
 func New(f Func) *Memo {
-	memo := &Memo{requests: make(chan request)}
+	memo := &Memo{make(chan request), make(chan request)}
 	go memo.server(f)
 	return memo
 }
@@ -39,7 +46,6 @@ func (memo *Memo) Get(key string, done <-chan struct{}) (interface{}, error) {
 		memo.cancels <- req
 	default:
 	}
-
 	return res.value, res.err
 }
 
@@ -47,19 +53,39 @@ func (memo *Memo) Close() { close(memo.requests) }
 
 func (memo *Memo) server(f Func) {
 	cache := make(map[string]*entry)
-	for req := range memo.requests {
-		e := cache[req.key]
-		if e == nil {
-			e = &entry{ready: make(chan struct{})}
-			cache[req.key] = e
-			go e.call(f, req.key, req.done)
+Loop:
+	for {
+	Cancel:
+		for {
+			select {
+			case req := <-memo.cancels:
+				fmt.Println("server: deleting cancelled entry (early)")
+				delete(cache, req.key)
+			default:
+				break Cancel
+			}
 		}
-		go e.deliver(req.response)
+		select {
+		case req := <-memo.cancels:
+			fmt.Println("server: deleting cancelled entry")
+			delete(cache, req.key)
+			continue Loop
+		case req := <-memo.requests:
+			fmt.Println("server: request")
+			e := cache[req.key]
+			if e == nil {
+				e = &entry{ready: make(chan struct{})}
+				cache[req.key] = e
+				go e.call(f, req.key, req.done)
+			}
+			go e.deliver(req.response)
+		}
 	}
 }
 
 func (e *entry) call(f Func, key string, done <-chan struct{}) {
 	e.res.value, e.res.err = f(key, done)
+	fmt.Println("call: returned from f")
 	close(e.ready)
 }
 
