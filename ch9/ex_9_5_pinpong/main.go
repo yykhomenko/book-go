bc51d5e90a23298a063d4043dd4aaa63597aeb7f
yykhomenko <yykhomenko@gmail.com>
2020-11-10 20:28:25 +0200

    ex9.5 done

--- ch9/ex_9_5_pinpong/main.go
+++ ch9/ex_9_5_pinpong/main.go
@@ -1,15 +1,38 @@
+// go 1.15.3, 2.9 GHz Quad-Core Intel Core i7
+// messages per second: 2300119
 package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+var counter uint64
 
 func main() {
 	pinc := make(chan string)
 	pongc := make(chan string)
-
-	go func() {
-		pinc <- "pin"
-	}()
-
+	go start(pinc)
+	go stats()
 	go pin(pinc, pongc)
 	pong(pongc, pinc)
+}
+
+func start(pinc chan string) {
+	pinc <- "pin"
+}
+
+func stats() {
+	ticker := time.Tick(1 * time.Second)
+	for {
+		select {
+		case <-ticker:
+			fmt.Printf("messages per second: %d\n", counter)
+			for !atomic.CompareAndSwapUint64(&counter, counter, 0) {
+			}
+		}
+	}
 }
 
 func pin(out chan<- string, in <-chan string) {
@@ -20,6 +43,7 @@ func pin(out chan<- string, in <-chan string) {
 
 func pong(out chan<- string, in <-chan string) {
 	for range in {
+		atomic.AddUint64(&counter, 1)
 		out <- "pong"
 	}
 }
