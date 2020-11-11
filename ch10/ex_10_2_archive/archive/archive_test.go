3cfb7e5fdf54cc82738ebf44894713f86f9359b3
yykhomenko <yykhomenko@gmail.com>
2020-11-11 15:12:12 +0200

    remove ex10.2 test

--- ch10/ex_10_2_archive/archive/archive_test.go
+++ ch10/ex_10_2_archive/archive/archive_test.go
@@ -1,9 +1,6 @@
 package archive_test
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"os"
 	"testing"
 
@@ -27,24 +24,4 @@ func Test_sniff(t *testing.T) {
 	}
 
 	assert.Equal(t, "tar", kind)
-}
-
-func Test_ReadZip(t *testing.T) {
-	f, err := os.Open("zip/testdata/test.zip")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	r, _, err := archive.NewReader(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	w := &bytes.Buffer{}
-	if _, err := io.Copy(w, r); err != nil {
-
-	}
-
-	fmt.Println(w.String())
 }
