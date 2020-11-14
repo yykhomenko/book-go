da8bb4eef95f98a54f87a426574721611f1bcfd9
yykhomenko <yykhomenko@gmail.com>
2020-11-14 22:14:24 +0200

    added 11.3 impl

--- ch11/ex_11_3_palindrome/word_test.go
+++ ch11/ex_11_3_palindrome/word_test.go
@@ -33,17 +33,6 @@ func TestIsPalindrome(t *testing.T) {
 	}
 }
 
-func randomPalindrome(rnd *rand.Rand) string {
-	n := rnd.Intn(25)
-	runes := make([]rune, n)
-	for i := 0; i < (n+1)/2; i++ {
-		r := rune(rnd.Intn(0x1000))
-		runes[i] = r
-		runes[n-1-i] = r
-	}
-	return string(runes)
-}
-
 func TestRandomPalindromes(t *testing.T) {
 	seed := time.Now().UTC().UnixNano()
 	t.Logf("random generator seed: %d", seed)
@@ -56,6 +45,35 @@ func TestRandomPalindromes(t *testing.T) {
 	}
 }
 
-// func TestRandomNonPalindromes(t *testing.T) {
-//
-// }
+func TestRandomNonPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("random generator seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomNonPalindrome(rng)
+		if IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = true", p)
+		}
+	}
+}
+
+func randomPalindrome(rnd *rand.Rand) string {
+	n := rnd.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rnd.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func randomNonPalindrome(rnd *rand.Rand) string {
+	n := rnd.Intn(23) + 10
+	runes := make([]rune, n)
+	for i := 0; i < n; i++ {
+		r := rune(rnd.Intn(0x1000))
+		runes[i] = r
+	}
+	return string(runes)
+}
