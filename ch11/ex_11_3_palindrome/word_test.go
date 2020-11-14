55fefb065a099af129906b6480d2708b56fec7bd
yykhomenko <yykhomenko@gmail.com>
2020-11-14 20:50:53 +0200

    added 11.3 base

---
+++ ch11/ex_11_3_palindrome/word_test.go
@@ -0,0 +1,61 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false},
+		{"desserts", false},
+	}
+
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v\n", test.input, got)
+		}
+	}
+}
+
+func randomPalindrome(rnd *rand.Rand) string {
+	n := rnd.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rnd.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("random generator seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+// func TestRandomNonPalindromes(t *testing.T) {
+//
+// }
