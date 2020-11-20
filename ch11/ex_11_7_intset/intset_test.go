fb3d2b78546901361f8ca3168fd8ac9ee3636910
yykhomenko <yykhomenko@gmail.com>
2020-11-20 19:11:49 +0200

    done ex11.7

--- ch11/ex_11_7_intset/intset_test.go
+++ ch11/ex_11_7_intset/intset_test.go
@@ -10,6 +10,8 @@ import (
 const maxRandInt = 1e8
 
 var IntSet = new(intset.IntSet)
+var MapSet = make(map[int]struct{})
+var Has bool
 var Len int
 
 func BenchmarkAdd(b *testing.B) {
@@ -26,7 +28,7 @@ func BenchmarkAddAll(b *testing.B) {
 
 func BenchmarkHas(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		IntSet.Has(rand.Intn(maxRandInt))
+		Has = IntSet.Has(rand.Intn(maxRandInt))
 	}
 }
 
@@ -45,5 +47,29 @@ func BenchmarkCopy(b *testing.B) {
 func BenchmarkRemove(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IntSet.Remove(rand.Intn(maxRandInt))
+	}
+}
+
+func BenchmarkMapAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MapSet[rand.Intn(maxRandInt)] = struct{}{}
+	}
+}
+
+func BenchmarkMapHas(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, Has = MapSet[rand.Intn(maxRandInt)]
+	}
+}
+
+func BenchmarkMapLen(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Len = len(MapSet)
+	}
+}
+
+func BenchmarkMapRemove(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		delete(MapSet, rand.Intn(maxRandInt))
 	}
 }
