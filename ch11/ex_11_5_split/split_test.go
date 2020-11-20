97bca22516224c82ae700288b99baf18acfc7ac4
yykhomenko <yykhomenko@gmail.com>
2020-11-20 17:29:48 +0200

    done ex11.5

--- ch11/ex_11_5_split/split_test.go
+++ ch11/ex_11_5_split/split_test.go
@@ -6,9 +6,18 @@ import (
 )
 
 func TestSplit(t *testing.T) {
-	s, sep := "a:b:c", ":"
-	words := strings.Split(s, sep)
-	if got, want := len(words), 3; got != want {
-		t.Errorf("Split(%q, %q) returns %d words, want %d", s, sep, got, want)
+	tests := []struct {
+		s    string
+		sep  string
+		want int
+	}{
+		{"", ";", 1},
+		{"a:b:c", ":", 3},
+	}
+	for _, test := range tests {
+		words := strings.Split(test.s, test.sep)
+		if got := len(words); got != test.want {
+			t.Errorf("Split(%q, %q) returns %d words, want %d", test.s, test.sep, got, test.want)
+		}
 	}
 }
