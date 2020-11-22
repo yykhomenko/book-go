396b1c81bb12ebff7e15ca2397eb28720ff5070c
yykhomenko <yykhomenko@gmail.com>
2020-11-22 19:30:32 +0200

    added ex12.5 uint impl

--- ch12/ex_12_5_json/json.go
+++ ch12/ex_12_5_json/json.go
@@ -45,6 +45,9 @@ func encode(buf *bytes.Buffer, v reflect.Value) error {
 	case reflect.Int, reflect.Int8,
 		reflect.Int16, reflect.Int32, reflect.Int64:
 		buf.WriteString(strconv.FormatInt(v.Int(), 10))
+	case reflect.Uint, reflect.Uint8,
+		reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		buf.WriteString(strconv.FormatInt(v.Int(), 10))
 	default:
 		return fmt.Errorf("unsupported type: %q", v.Kind())
 	}
