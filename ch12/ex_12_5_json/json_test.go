974f7298cfb28b5b50bb0b4c057b93cc7ff741a6
yykhomenko <yykhomenko@gmail.com>
2020-11-22 19:42:31 +0200

    added ex12.5 fix float test

--- ch12/ex_12_5_json/json_test.go
+++ ch12/ex_12_5_json/json_test.go
@@ -14,7 +14,7 @@ func TestMarshal(t *testing.T) {
 		{struct{}{}, "{}"},
 		{[]struct{}{}, "[]"},
 		{[]struct{ a int }{{-1}, {2}}, `[{"a":-1},{"a":2}]`},
-		{[]struct{ a, b float64 }{{-1.15, 2.5}}, `[{"a":-1.15},{"a":2.5}]`},
+		{[]struct{ a, b float64 }{{-1.15, 2.5}}, `[{"a":-1.15,"b":2.5}]`},
 	}
 	for _, test := range tests {
 		got, err := Marshal(test.v)
