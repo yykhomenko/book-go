17a1e754ca0f3b37ee45695e55aeddfadd5243ba
yykhomenko <yykhomenko@gmail.com>
2020-11-22 19:28:04 +0200

    added ex12.5 struct, array and int

--- ch12/ex_12_5_json/json.go
+++ ch12/ex_12_5_json/json.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func Marshal(v interface{}) ([]byte, error) {
@@ -18,12 +19,32 @@ func encode(buf *bytes.Buffer, v reflect.Value) error {
 	switch v.Kind() {
 	case reflect.Struct:
 		buf.WriteByte('{')
-
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteByte(',')
+			}
+			buf.WriteByte('"')
+			buf.WriteString(v.Type().Field(i).Name)
+			buf.WriteString(`":`)
+			if err := encode(buf, v.Field(i)); err != nil {
+				return err
+			}
+		}
 		buf.WriteByte('}')
 	case reflect.Array, reflect.Slice:
 		buf.WriteByte('[')
-
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteByte(',')
+			}
+			if err := encode(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
 		buf.WriteByte(']')
+	case reflect.Int, reflect.Int8,
+		reflect.Int16, reflect.Int32, reflect.Int64:
+		buf.WriteString(strconv.FormatInt(v.Int(), 10))
 	default:
 		return fmt.Errorf("unsupported type: %q", v.Kind())
 	}
