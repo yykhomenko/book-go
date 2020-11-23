ce58f8b4251d13794768054e78a14760b99e6c77
yykhomenko <yykhomenko@gmail.com>
2020-11-23 23:23:13 +0200

    added read

--- ch12/sexpr/decode.go
+++ ch12/sexpr/decode.go
@@ -1,1 +1,61 @@
 package sexpr
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"text/scanner"
+)
+
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		if lex.text() == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text())
+		v.SetString(s)
+		lex.next()
+		return
+	case scanner.Int:
+		i, _ := strconv.Atoi(lex.text())
+		v.SetInt(int64(i))
+		lex.next()
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next()
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array:
+		for i := 0; !endlist(lex); i++ {
+			read(lex, v.Index(i))
+		}
+	case reflect.Slice:
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+	case reflect.Struct:
+		for !endList(lex) {
+			lex.consume('(')
+			if lex.token != scanner.Ident {
+				panic(fmt.Sprintf("got token %q, want field name", lex.text()))
+			}
+			name := lex.text()
+			lex.next()
+			read(lex, v.FieldByName(name))
+			lex.consume(')')
+		}
+	}
+}
